04bab8a8f8c268f8c9067eff60d18cfe9c1874c0
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-04-18 13:23:34 -0300

    start server

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json()) // JSON response
+
+// CORS
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173'
+}))
+
+// Archive folder
+app.use(express.static('public'))
+
+// Routes
+
+// Server
+app.listen(5000)
